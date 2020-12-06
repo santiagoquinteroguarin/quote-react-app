1c4532e752578838fd682ca416ccc79d8af0a724
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-06 18:23:35 -0500

    Creating Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
+// ?1.
+import styled from '@emotion/styled';
+
+// ?2.
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Fragment>
-      <Header titleHeader="Cotizador de Seguros"/>
-    </Fragment>
+    // ?3.
+    <Container>
+      <Header 
+        titleHeader="Cotizador de Seguros"
+      />
+      
+      <ContainerForm>
+        <Form/>
+      </ContainerForm>
+    </Container>
   );
 }
 
