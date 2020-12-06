9bdde005081dfea35f42e0ac42268cff8bcd6d13
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-06 18:06:12 -0500

    Adding styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+// ?1.
+import styled from '@emotion/styled';
+
+// ?2.
+const ContainerHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
 const Header = ({titleHeader}) => {
     return (
-        <header>
-            <h1>{titleHeader}</h1>
-        </header>
+        // ?3.
+        <ContainerHeader>
+            <TextHeader>{titleHeader}</TextHeader>
+        </ContainerHeader>
     );
 }
 
