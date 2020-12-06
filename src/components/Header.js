3feee3c0690b30dd20bbafe186d641ca4baf75c3
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-06 17:51:03 -0500

    development environment configuration

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({titleHeader}) => {
+    return (
+        <header>
+            <h1>{titleHeader}</h1>
+        </header>
+    );
+}
+
+export default Header;
